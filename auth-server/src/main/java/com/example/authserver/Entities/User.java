bf73b6573de048f808f444463775e59e7faa9b1a
kwistprod <kwist879@gmail.com>
2021-05-26 00:12:48 +0300

    added user entity

---
+++ auth-server/src/main/java/com/example/authserver/Entities/User.java
@@ -0,0 +1,19 @@
+package com.example.authserver.Entities;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity(name="users")
+@Data
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column
+    private String username;
+    @Column
+    private String password;
+}
