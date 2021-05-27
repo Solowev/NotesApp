1dc7aa59b7f2152c6ffc4294f04e1e51c85e6b90
kwistprod <kwist879@gmail.com>
2021-05-27 12:46:22 +0300

    Created email service

--- auth-server/src/main/java/com/example/authserver/Utils/EmailSenderUtil.java
+++ auth-server/src/main/java/com/example/authserver/Utils/EmailSenderUtil.java
@@ -1,4 +1,28 @@
 package com.example.authserver.Utils;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Component;
+
+
+@Component
 public class EmailSenderUtil {
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    public void sendMessage(String to, String subject, String text){
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("noreply@notesapp.ru");
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+        try{
+            javaMailSender.send(message);
+        }catch(MailException ex){
+            ex.printStackTrace();
+        }
+    }
 }
