a9b2ff7b1bd8c32842800b43a9d27345b4f7302e
kwistprod <kwist879@gmail.com>
2021-06-01 18:10:39 +0300

    created initial filter

---
+++ main-server/src/main/java/com/example/mainserver/Authentication/Filters/InitialAuthenticationFilter.java
@@ -0,0 +1,27 @@
+package com.example.mainserver.Authentication.Filters;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class InitialAuthenticationFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private AuthenticationManager manager;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+        return !request.getServletPath().equals("/login");
+    }
+}
